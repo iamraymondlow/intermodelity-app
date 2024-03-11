4ef755a8ee2f593cd613985cbcd0b2c224432c9f
Raymond Low <iamraymondlow@gmail.com>
2024-03-11 16:15:27 +0000

    Fixed incorrect app name in start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -4,12 +4,13 @@
 # Enter you AWS account ID and region
 AWS_DEFAULT_REGION='us-east-1'
 AWS_ACCOUNT_ID='858486579140'
+CONTAINER_NAME='intermodelity-app'
 
 # ECR login
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 
 # pull the new image
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/xtb-mo-st-app:latest
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER_NAME:latest
 
 # run the container
-docker run -d -p 8501:8501 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/xtb-mo-st-app:latest
+docker run -d -p 8501:8501 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER_NAME:latest
